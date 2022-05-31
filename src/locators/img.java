efa804ef8b503f44f69475b805fe9dfb909acd99
sugiarun <gankeerthi09@gmail.com>
2022-05-31 15:04:35 +0530

    locate to git

---
+++ src/locators/img.java
@@ -0,0 +1,18 @@
+package locators;
+
+
+
+public class img {
+	public void fstfloor() {
+		System.out.println("haritha");
+		System.out.println('i');
+		System.out.println("female");
+	}
+	public static void main(String[]args) {
+		img i=new img();
+		i.fstfloor();
+
+	}
+
+
+}
